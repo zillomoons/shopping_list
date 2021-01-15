495b8f4f3439ec4e6cc19c91b8ff4194b60ebb4e
zillomoons <dlzht73@ya.ru>
2021-01-15 07:35:37 +0300

    adda js

--- main.js
+++ main.js
@@ -15,9 +15,11 @@ for (let i = 0; i < listItems.length; i++) {
     })
 
     item.addEventListener('dragend', function () {
-        //setTimeout
-        draggedItem.style.display = 'block';
-        draggedItem = null;
+        setTimeout(function () {
+            draggedItem.style.display = 'block';
+            draggedItem = null;
+        }, 0)
+
     })
 
     for (let j = 0; j < lists.length; j++) {
@@ -26,10 +28,17 @@ for (let i = 0; i < listItems.length; i++) {
         list.addEventListener('dragover', function (e) {
             e.preventDefault();
         })
-
+        list.addEventListener('dragenter', function (e) {
+            e.preventDefault();
+            this.style.backgroundColor = '#e6d1d1';
+        })
+        list.addEventListener('dragleave', function (e) {
+            this.style.backgroundColor = '#f3f3f3';
+        })
         list.addEventListener('drop', function (e) {
             e.preventDefault();
             this.append(draggedItem);
+            this.style.backgroundColor = '#f3f3f3';
         })
 
     }
